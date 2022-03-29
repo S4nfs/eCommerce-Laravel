8206165d2e838abb9b1d81afd42dcc4e12ed7331
S4nfs <zyzzaestheticsgods@gmail.com>
2022-03-29 21:19:23 +0530

    items arranged as per smaller devices

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -13,15 +13,17 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Orders</a>
                 </li>
-                <li class="nav-item">
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </li>
+                <li><a class="nav-link" href="#">Cart</a></li>
+
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a class="nav-link" href="#">Cart</a></li>
+              
+              <li class="nav-item">
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </li>
             </ul>
         </div>
     </div>
