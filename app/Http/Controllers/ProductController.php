66b1cf4a1518634a41c4719e759f512669791cd0
S4nfs <zyzzaestheticsgods@gmail.com>
2022-03-30 22:17:12 +0530

    fetching products in an array

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     function index(){
-        return "Product Page";
+        $data = Product::all();
+        return view('product', ['products' => $data]);
     }
 }
