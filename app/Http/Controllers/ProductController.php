867e76db1a217fad19242eff4af2483ef1918e4f
S4nfs <zyzzaestheticsgods@gmail.com>
2022-04-01 15:08:25 +0530

    addtocart function to store the added product details

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,16 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
 
 class ProductController extends Controller
 {
-    function index(){
+    function index()
+    {
         $data = Product::all();
         return view('product', ['products' => $data]);
     }
 
-    function productDetail($id){
+    function productDetail($id)
+    {
         $data = Product::find($id);
         return view('detail', ['product' => $data]);
+    }
+    function addToCart(Request $req)
+    {
+        if ($req->session()->has('user')) {
+            $cart = new Cart;   
+            $cart->user_id = $req->session()->get('user')[0]->id;
+            $cart->product_id = $req->product_id;
+            $cart->save();
+
+        } else {
+            return redirect('/login');
+        }
     }
 }
