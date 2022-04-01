ea8b9646139bd9250a8b3617d0ec4007df844b98
S4nfs <zyzzaestheticsgods@gmail.com>
2022-04-01 22:28:51 +0530

    joining cart and products databse to fetch user based product details

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
 use Session;
+use Illuminate\Support\Facades\DB;
 class ProductController extends Controller
 {
     function index()
@@ -35,5 +36,14 @@ class ProductController extends Controller
     function countCartItem(){
         $userId = Session::get('user')[0]->id;
         return Cart::where('user_id', $userId)->count();
+    }
+
+    function mycartlist(){
+        $userId = Session()->get('user')[0]->id;
+        $fetchAllCart = DB::table('cart')->join('products', 'cart.product_id', '=', 'products.id')->where('cart.user_id', $userId)->select('products.*')->get();
+
+        return view('cartlist', ['products' => $fetchAllCart]);
+
+    
     }
 }
