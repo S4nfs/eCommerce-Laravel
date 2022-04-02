7c11fe718f37fda55fc49bc94dc9fd4eae65e6e3
S4nfs <zyzzaestheticsgods@gmail.com>
2022-04-02 15:20:47 +0530

    remove cart product by using cart's main id

--- resources/views/cartlist.blade.php
+++ resources/views/cartlist.blade.php
@@ -9,17 +9,20 @@
                 <th scope="col"></th>
                 <th scope="col"></th>
                 <th scope="col"></th>
+                <th scope="col"></th>
+                <th scope="col"></th>
             </tr>
         </thead>
 
         @foreach ($products as $items)
             <tr>
-                <th scope="row">1</th>
+                {{-- <th scope="row">1</th> --}}
                 <td><img class="trending-img" src="{{ $items->gallery }}"></td>
                 <td>{{ $items->name }}</td>
                 <td>{{ $items->description }}</td>
                 <td>Rs. {{ $items->price }}</td>
-                <td colspan="2"><button type="button" class="btn btn-warning">Remove</button></td>
+                <td colspan="2"><a href="/removecart/{{$items->cart_id}}" class="btn btn-warning">Remove to Cart</a></td>
+
             </tr>
         @endforeach
         </tbody>
