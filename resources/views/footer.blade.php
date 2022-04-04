11c1dce893b88a3e5eddbb6a190b6dbf97a7632d
S4nfs <zyzzaestheticsgods@gmail.com>
2022-04-04 23:10:34 +0530

    footer with links and resources

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -7,20 +7,27 @@
         <div class="row">
           <!--Grid column-->
           <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-            <h5 class="text-uppercase">Links</h5>
+            <h5 class="text-uppercase">S-Mart</h5>
+  <p>Shop from a vast range of 20,000+ products including farm-fresh fruits and vegetables, groceries, fish, chicken, meat, home & household essentials, pet care needs, imported wines from disneyland at the best prices. Enjoy hassle-free online grocery shopping at just a click of a button</p>
+          </div>
+          <!--Grid column-->
+  
+          <!--Grid column-->
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Browse</h5>
   
             <ul class="list-unstyled mb-0">
               <li>
-                <a href="#!" class="text-white">Link 1</a>
+                <a href="/" class="text-white">Products</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 2</a>
+                <a href="/cartlist" class="text-white">Cart</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 3</a>
+                <a href="/ordernow" class="text-white">Orders</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 4</a>
+                <a href="#!" class="text-white">Home Delivery</a>
               </li>
             </ul>
           </div>
@@ -32,37 +39,16 @@
   
             <ul class="list-unstyled mb-0">
               <li>
-                <a href="#!" class="text-white">Link 1</a>
+                <a href="#!" class="text-white">Shop</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 2</a>
+                <a href="#!" class="text-white">Career</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 3</a>
+                <a href="#!" class="text-white">Projects</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 4</a>
-              </li>
-            </ul>
-          </div>
-          <!--Grid column-->
-  
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-            <h5 class="text-uppercase">Links</h5>
-  
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a href="#!" class="text-white">Link 1</a>
-              </li>
-              <li>
-                <a href="#!" class="text-white">Link 2</a>
-              </li>
-              <li>
-                <a href="#!" class="text-white">Link 3</a>
-              </li>
-              <li>
-                <a href="#!" class="text-white">Link 4</a>
+                <a href="#!" class="text-white">Partners</a>
               </li>
             </ul>
           </div>
@@ -70,20 +56,20 @@
   
           <!--Grid column-->
           <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-            <h5 class="text-uppercase">Links</h5>
+            <h5 class="text-uppercase">Resources</h5>
   
             <ul class="list-unstyled mb-0">
               <li>
-                <a href="#!" class="text-white">Link 1</a>
+                <a href="#!" class="text-white">FSSAI License No. 00000000000000</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 2</a>
+                <a href="#!" class="text-white">Sitemap</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 3</a>
+                <a href="#!" class="text-white">Privacy Policy</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 4</a>
+                <a href="#!" class="text-white">Terms Of Use</a>
               </li>
             </ul>
           </div>
@@ -97,8 +83,8 @@
   
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2022 Copyright
-      <a class="text-white" href="https://github.com/S4nfs"> Made By S4nfs with Coffee and Laravel</a>
+      © 2022 Copyright Made By 
+      <a class="text-white" href="https://github.com/S4nfs">S4nfs</a> with <img src="https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-coffee-working-stress-flaticons-flat-flat-icons.png" style="width: 1.5rem;"/> and Laravel
     </div>
     <!-- Copyright -->
   </footer>
