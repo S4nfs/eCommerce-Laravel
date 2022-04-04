2f246b952dbc2e231ed91b12a99c7891f9b22253
S4nfs <zyzzaestheticsgods@gmail.com>
2022-04-04 23:07:18 +0530

    register user with hash password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    function registeruser(Request $req){
+        $data = new User;   //new instance or object
+        $data->name = $req->name;
+        $data->email = $req->email;
+        $data->password = Hash::make($req->password);
+        $data->save();
+        return redirect('/login');
+    }
+// ------------------------------------------------------------------
     function login(Request $req)
     {
         $data = User::where(['email' => $req->email])->get();
