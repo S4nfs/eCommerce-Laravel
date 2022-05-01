af8370273131b111acb2a4dae75dda65aedf6aa0
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-01 14:42:26 +0530

    alerts

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,25 +1,9 @@
 @extends('master')
 @section('content')
 
-@if(session()->has('message'))
-{{-- <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
-    <div class="toast" style="position: absolute; top: 0; right: 0;">
-      <div class="toast-header">
-        <img src="..." class="rounded mr-2" alt="...">
-        <strong class="mr-auto">Bootstrap</strong>
-        <small>11 mins ago</small>
-        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="toast-body">
-        {{session()->get('message')}}
-      </div>
-    </div>
-  </div> --}}
-  <div class="alert alert-success">{{session()->get('message')}}</div>
-
-  @endif
+{{-- alert from componenet --}}
+<x-alert>
+</x-alert>
 <div class="container emp-profile">
     <form method="post" action="/avatarupload" enctype="multipart/form-data">
         @csrf
