42c903f3602ead309733a1cdb6ba45c4cc9deeb5
S4nfs <zyzzaestheticsgods@gmail.com>
2022-06-03 20:28:51 +0530

    bootstrap icons cdn

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,14 +9,15 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href='/css/login.css'>
     <link rel="stylesheet" href="{{ asset('/css/order.css') }}">
     <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
-<link rel="stylesheet" href="{{asset('css/profile.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
     {{ View::make('header') }}
-
     <title>S-Mart Shopping Platform</title>
 </head>
+
 <body>
     @yield('content')
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
